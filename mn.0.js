072ad32248703ecb2870463ef41effdf58445abf
Carlos Mario <crlsmrsalazar@gmail.com>
2024-11-20 13:36:02 -0500

    ejercicios javaScript Carlos Mario

---
+++ mn.0.js
@@ -0,0 +1,5 @@
+import { calcularDescuento } from './utilis.0.js';
+
+console.log(calcularDescuento(1200)); 
+console.log(calcularDescuento(800));  
+console.log(calcularDescuento(300));  
---
+++ mn.1.js
@@ -0,0 +1,7 @@
+import { enRango } from './utili.1.js';
+
+console.log(enRango(15));  
+console.log(enRango(5));   
+console.log(enRango(50));  
+console.log(enRango(55));  
+console.log(enRango(10));  
